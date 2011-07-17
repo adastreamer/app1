8deacc7e97787399fa95b7834e5f251b38614ae7
AdaStreamer <AdaStreamer@gmail.com>
2011-07-17 18:44:45 +0400

    Commit #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '3cdcbf82fa3863e3bf81cbd874ca33748e8d893aa85a8d6a31cdf42f74d64b1b69808a1811514e6726e3f3a9a423220d5ad920c16dfff7ca18890c9e91630266'
